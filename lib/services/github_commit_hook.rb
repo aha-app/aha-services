7e98e66a4644bcddc918c04e060e41a795541de3
k1w1 <k1w1@k1w1.org>
2013-05-30 12:03:13 -0700

    * Working on github commit hook.

--- lib/services/github_commit_hook.rb
+++ lib/services/github_commit_hook.rb
@@ -1,12 +1,9 @@
 class AhaServices::GithubCommitHook < AhaService
   callback_url
 
+  # Create a comment for each commit where the message contains a feature
+  # or requirement ID.
   def receive_webhook
-    puts "Got webhook"
-    puts payload.inspect
-    
-    # Create a comment for each commit where the message contains a feature
-    # or requirement ID.
     payload.commits.each do |commit|
       commit.message
       
@@ -21,8 +18,21 @@ class AhaServices::GithubCommitHook < AhaService
 protected
 
   def comment_on_record(ref_num, commit)
-    record_type = ref_num =~ /-R-/ ? "requirements" : "features"
-    api.create_comment(record_type, ref_num, "commit")
+    record_type = ref_num =~ /-[0-9]+-/ ? "requirements" : "features"
+    
+    message = <<EOF
+<p>#{commit.committer.name} (#{commit.committer.email}) committed to <a href="#{payload.repository.url}">#{payload.repository.name}</a>:</p>
+<pre>#{commit.message}</pre>
+<p>Commit: <a href="#{commit.url}">#{commit.url}</a></p>
+EOF
+
+    begin
+      api.create_comment(record_type, ref_num, message)
+    rescue AhaApi::NotFound
+      # Ignore errors for unknown references - it might not have really
+      # been a reference.
+      logger.warn("No record found for reference: #{ref_num}")
+    end
   end
   
 end
--- spec/services/github_commit_hook_spec.rb
+++ spec/services/github_commit_hook_spec.rb
@@ -3,9 +3,24 @@ require 'spec_helper'
 describe AhaServices::GithubCommitHook do
   it "can create comments" do
     # Call into Aha!
-    stub_request(:post, "https://a.aha.io/api/v1/features/OPS-11/comments").
-      with(:body => {:integration_field => {:name => "id", :value => "10009"}}).
+    stub_request(:post, "https://a.aha.io/api/v1/features/SCRATCH-34/comments").
+      with(:body => {:comment => {:body => /committed/}}).
       to_return(:status => 201, :body => "", :headers => {})
+    stub_request(:post, "https://a.aha.io/api/v1/features/AA-12/comments").
+      with(:body => {:comment => {:body => /committed/}}).
+      to_return(:status => 201, :body => "", :headers => {})
+    stub_request(:post, "https://a.aha.io/api/v1/requirements/BB-13-1/comments").
+      with(:body => {:comment => {:body => /committed/}}).
+      to_return(:status => 201, :body => "", :headers => {})
+      
+    AhaServices::GithubCommitHook.new(:webhook,
+      {},json_fixture('github_commit_hook_webhook.json')).receive
+  end
+  
+  it "silently ignores invalid references" do
+    # Call into Aha!
+    stub_request(:post, //).
+      to_return(:status => 404, :body => "", :headers => {})
       
     AhaServices::GithubCommitHook.new(:webhook,
       {},json_fixture('github_commit_hook_webhook.json')).receive
