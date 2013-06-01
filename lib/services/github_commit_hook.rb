fd5f01cf059f3cb8929c407d7b4ed995a8a7376f
k1w1 <k1w1@k1w1.org>
2013-06-01 11:37:57 -0700

    * Added debugging for OPS-12.

--- lib/services/github_commit_hook.rb
+++ lib/services/github_commit_hook.rb
@@ -24,6 +24,8 @@ protected
   def comment_on_record(ref_num, commit)
     record_type = ref_num =~ /-[0-9]+-/ ? "requirements" : "features"
     
+    logger.debug("COMMIT: #{commit.inspect}")
+    
     message = <<EOF
 <p>#{commit.committer.name} (#{commit.committer.email}) committed to <a href="#{payload.repository.url}">#{payload.repository.name}</a>:</p>
 <blockquote><pre>#{commit.message}</pre></blockquote>
