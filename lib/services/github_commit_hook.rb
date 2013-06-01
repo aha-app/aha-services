cac63b1796848f81332f0df062d1c32451e64453
k1w1 <k1w1@k1w1.org>
2013-06-01 11:41:39 -0700

    * Fixed reference to payload.

--- lib/services/github_commit_hook.rb
+++ lib/services/github_commit_hook.rb
@@ -13,7 +13,7 @@ class AhaServices::GithubCommitHook < AhaService
       
       commit.message.scan(/([A-Z]+-[0-9]+(?:-[0-9]+)?)/) do |m|
         m.each do |ref|
-          comment_on_record(ref, commit)
+          comment_on_record(commit_payload, ref, commit)
         end
       end
     end
@@ -21,14 +21,11 @@ class AhaServices::GithubCommitHook < AhaService
   
 protected
 
-  def comment_on_record(ref_num, commit)
+  def comment_on_record(commit_payload, ref_num, commit)
     record_type = ref_num =~ /-[0-9]+-/ ? "requirements" : "features"
     
-    logger.debug("COMMIT: #{commit.inspect}")
-    logger.debug("URL: #{commit.url}")
-    
     message = <<EOF
-<p>#{commit.committer.name} (#{commit.committer.email}) committed to <a href="#{payload.repository.url}">#{payload.repository.name}</a>:</p>
+<p>#{commit.committer.name} (#{commit.committer.email}) committed to <a href="#{commit_payload.repository.url}">#{commit_payload.repository.name}</a>:</p>
 <blockquote><pre>#{commit.message}</pre></blockquote>
 <p>Commit: <a href="#{commit.url}">#{commit.url}</a></p>
 EOF
