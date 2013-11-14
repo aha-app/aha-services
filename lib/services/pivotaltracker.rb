445a03124467e7029c65ce81121b6d660503b699
lightsuner <lightsuner@gmail.com>
2013-11-14 18:28:45 +0300

    add some integration fields with aha

--- lib/services/pivotaltracker.rb
+++ lib/services/pivotaltracker.rb
@@ -1,73 +1,27 @@
 class AhaServices::Pivotaltracker < AhaService
-  string :server_url, description: "URL for the Jira server, without the trailing slash, e.g. https://bigaha.atlassian.net"
-  string :username
-  password :password
+  string :api_token, description: "Api token from pivotaltracker.com"
   install_button
-  select :project, collection: ->(meta_data, data) { meta_data.projects.collect{|p| [p.name, p['key']] } }
-  internal :feature_status_mapping
-  internal :resolution_mapping
-
-  callback_url description: "URL to add to the webhooks section of Jira."
+  select :project, collection: ->(meta_data, data) { meta_data.projects.collect{|p| [p.name, p.id] } }
+  callback_url description: "URL to add to the webhooks section of Pivotaltracker."
 
   @@api_url = 'https://www.pivotaltracker.com/services/v5'
 
   def receive_installed
     prepare_request
-    response = http_get '%s/rest/api/2/issue/createmeta' % [data.server_url]
-    projects = []
-    process_response(response, 200) do |meta|
-      meta['projects'].each do |project|
-        issue_types = []
-
-        # Get the statuses.
-        status_response = http_get '%s/rest/api/2/project/%s/statuses' % [data.server_url, project['key']]
-        if status_response.status == 404
-          # In Jira 5.0 the status is not associated with each issue type.
-          status_response = http_get '%s/rest/api/2/status' % [data.server_url]
-          process_response(status_response, 200) do |status_meta|
-            statuses = []
-            status_meta.each do |status|
-              statuses << {:id => status['id'], :name => status['name']}
-            end
-
-            project['issuetypes'].each do |issue_type|
-              issue_types << {:id => issue_type['id'], :name => issue_type['name'],
-                              :subtask => issue_type['subtask'], :statuses => statuses}
-            end
-          end
-        else
-          process_response(status_response, 200) do |status_meta|
-            status_meta.each do |issue_type|
-              statuses = []
-              issue_type['statuses'].each do |status|
-                statuses << {:id => status['id'], :name => status['name']}
-              end
-
-              issue_types << {:id => issue_type['id'], :name => issue_type['name'],
-                              :subtask => issue_type['subtask'], :statuses => statuses}
-            end
-          end
-        end
-
-        projects << {:id => project['id'], :key => project['key'],
-                     :name => project['name'], :issue_types => issue_types}
+    response = http_get '%s/projects' % [@@api_url]
+    available_projects = []
+    process_response(response, 200) do |projects|
+      projects.each do |project|
+        available_projects << {:id => project['id'], :name => project['name']}
       end
     end
-    @meta_data.projects = projects
-
-    response = http_get '%s/rest/api/2/resolution' % [data.server_url]
-    resolutions = []
-    process_response(response, 200) do |meta|
-      meta.each do |resolution|
-        resolutions << {:id => resolution['id'], :name => resolution['name']}
-      end
-    end
-    @meta_data.resolutions = resolutions
-
+    @meta_data.projects = available_projects
+    p  @meta_data.projects
   end
 
   def receive_create_feature
-    add_story payload.feature, data.project_id, nil
+    version_id = get_service_id(payload.feature.release.integration_fields)
+    add_story payload.feature, data.project, version_id
   end
 
   def add_story(resource, project_id, version_id)
@@ -77,7 +31,7 @@ class AhaServices::Pivotaltracker < AhaService
         name: resource.name,
         description: append_link(strip_html(resource.description.body), resource),
         cl_numbers: version_id,
-        #deadline:'',
+        #deadline: resource,
         #current_state:'',  #accepted, delivered, finished, started, rejected, unstarted, unscheduled
         story_type: 'feature', #feature, bug, chore, release
         created_at: resource.created_at,
@@ -91,8 +45,12 @@ class AhaServices::Pivotaltracker < AhaService
     response = http_post '%s/projects/%s/stories' % [@@api_url, project_id], story.to_json
 
     process_response(response, 200) do |new_story|
-      p new_story
-      #logger.info("Created issue #{issue_id} / #{issue_key}")
+      story_id = new_story['id']
+      story_url = new_story['url']
+      logger.info("Created story #{story_id}")
+
+      api.create_integration_field(resource.reference_num, self.class.service_name, :id, story_id)
+      api.create_integration_field(resource.reference_num, self.class.service_name, :url, story_url)
     end
 
     story_id
@@ -128,13 +86,17 @@ class AhaServices::Pivotaltracker < AhaService
   def process_response(response, *success_codes, &block)
     if success_codes.include?(response.status)
       yield parse(response.body)
-    elsif response.status == 401 || response.status == 403
-      raise AhaService::RemoteError, "Authentication failed: #{response.status} #{response.headers['X-Authentication-Denied-Reason']}"
-    elsif response.status == 400
+    elsif response.status == 404 ||  response.status == 403 ||  response.status ==  401 || response.status ==  400
       errors = parse(response.body)
-      error_string = errors["errorMessages"].join(", ") +
-          errors["errors"].map {|k, v| "#{k}: #{v}" }.join(", ")
-      raise AhaService::RemoteError, "Data not accepted: #{error_string}"
+      error_string = errors.error
+      if !errors.general_problem.nil?
+        error_string + ' ' + errors.general_problem
+      end
+      if !errors.possible_fix.nil?
+        error_string + ' ' + errors.possible_fix
+      end
+
+      raise AhaService::RemoteError, "Error code: #{errors.code} - #{error_string}"
     else
       raise AhaService::RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
     end
@@ -145,6 +107,19 @@ class AhaServices::Pivotaltracker < AhaService
       {}
     else
       JSON.parse(body)
+    end
+  end
+
+
+  def get_service_id(integration_fields)
+    return nil if integration_fields.nil?
+    field = integration_fields.detect do |f|
+      f.service_name == self.class.service_name and f.name == "id"
+    end
+    if field
+      field.value
+    else
+      nil
     end
   end
 
