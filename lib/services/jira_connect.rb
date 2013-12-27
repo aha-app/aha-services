38e02a904016a549450c5aaeccfe64d998241e91
k1w1 <k1w1@k1w1.org>
2013-12-27 11:11:38 -0800

    * Added missing requirement configuration to JIRA via Connect plugin.

--- lib/services/jira_connect.rb
+++ lib/services/jira_connect.rb
@@ -9,6 +9,10 @@ class AhaServices::JiraConnect < AhaServices::Jira
     collection: ->(meta_data, data) { 
       meta_data.projects.detect {|p| p['key'] == data.project}.issue_types.find_all{|i| !i['subtype']}.collect{|p| [p.name, p.id] } 
     }, description: "Issue type that will be used for Jira issues."
+  select :requirement_issue_type, 
+    collection: ->(meta_data, data) { 
+      meta_data.projects.detect {|p| p['key'] == data.project}.issue_types.find_all{|i| !i['subtype']}.collect{|p| [p.name, p.id] } 
+    }, description: "JIRA issue type that will be used when sending requirements. If you are using JIRA Agile then we recommend 'Story'."
   internal :feature_status_mapping
   internal :resolution_mapping
   
