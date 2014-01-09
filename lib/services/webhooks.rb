e5ddcfe91a0b727bf0caa01b6b27541af6e97afc
k1w1 <k1w1@k1w1.org>
2014-01-09 09:45:37 -0800

    * Make webhook respond to correct events.

--- lib/services/webhooks.rb
+++ lib/services/webhooks.rb
@@ -1,7 +1,20 @@
 class AhaServices::Webhooks < AhaService
   string :hook_url
   
-  def receive_event
+  def receive_create_feature
     http_post data.hook_url, payload.to_json
   end
+  
+  def receive_update_feature
+    http_post data.hook_url, payload.to_json
+  end
+
+  def receive_create_release
+    http_post data.hook_url, payload.to_json
+  end
+  
+  def receive_update_release
+    http_post data.hook_url, payload.to_json
+  end
+  
 end
