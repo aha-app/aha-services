238099770dc860a87ce6912e65c64a764a7e20e3
k1w1 <k1w1@k1w1.org>
2014-01-15 20:06:25 -0800

    * Use a 5 minute timeout for network requests. Some JIRA servers can take 4 minutes to retrieve the project list.

--- lib/aha-services/service.rb
+++ lib/aha-services/service.rb
@@ -52,7 +52,7 @@ class AhaService
   
   def self.default_http_options
     @@default_http_options ||= {
-      :request => {:timeout => 120, :open_timeout => 5},
+      :request => {:timeout => 310, :open_timeout => 5},
       :ssl => {:verify => false, :verify_depth => 5},
       :headers => {}
     }
@@ -88,7 +88,7 @@ class AhaService
       return
     end
     logger.info("Sending :#{@event} using #{self.class.title}")
-    timeout_sec = (timeout || 130).to_i
+    timeout_sec = (timeout || 310).to_i
     Timeout.timeout(timeout_sec, TimeoutError) do
       send(event_method)
     end
