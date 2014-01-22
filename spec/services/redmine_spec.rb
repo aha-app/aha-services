74b02d8a2a4d44757bedbbf2984b9b526aa6d20d
ellmo <ellmunzai@gmail.com>
2014-01-22 17:41:54 +0100

    describing title, name and schema for Redmine

--- spec/services/redmine_spec.rb
+++ spec/services/redmine_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe AhaServices::Redmine do
-  it 'is not ready' do
-    pending "barely started"
+  context 'class' do
+    let(:title) { 'Redmine' }
+    let(:service_name) { 'redmine_issues' }
+    let(:schema_fields) {
+      [
+        [:string, :project_name, {}],
+        [:string, :api_key, {}]
+      ]
+    }
+
+    it "has required title and name" do
+      expect(described_class.title).to eq title
+      expect(described_class.service_name).to eq service_name
+    end
+
+    it "has required schema fields" do
+      expect(described_class.schema).to eq schema_fields
+    end
   end
 end
