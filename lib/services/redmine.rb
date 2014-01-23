afda17d2163ab08731ee98e9039f5b34d34270da
ellmo <ellmunzai@gmail.com>
2014-01-23 17:07:55 +0100

    redmine service 'installed'

--- lib/services/redmine.rb
+++ lib/services/redmine.rb
@@ -2,6 +2,55 @@ class AhaServices::Redmine < AhaService
   title 'Redmine'
   service_name 'redmine_issues'
 
-  string :project_name
+  string :redmine_url
   string :api_key
+  select :project, collection: -> (meta_data, data) { meta_data.projects.collect { |p| [p.name, p.id] } },
+    description: "Redmine project that this Aha! product will integrate with."
+  select :integration,
+    collection: ->(meta_data, data) { meta_data.projects.detect {|p| p.id.to_s == data.project.to_s }.integrations.collect{|p| [p.name, p.id] } },
+    description: "Redmine integration that you added for Aha!"
+
+  def receive_installed
+    available_projects = []
+    # Get list of projects.
+    prepare_request
+    response = http_get("#{data.redmine_url}/projects.json")
+    process_response(response, 200) do |body|
+      body['projects'].each do |project|
+        available_projects << {
+          :id => project['id'],
+          :name => project['name'],
+        }
+      end
+    end
+    @meta_data.projects = available_projects
+  end
+
+private
+
+  def prepare_request
+    http.headers['Content-Type'] = 'application/json'
+    http.headers['X-Redmine-API-Key'] = data.api_key
+  end
+
+  def process_response(response, *success_codes, &block)
+    if success_codes.include?(response.status)
+      yield parse(response.body)
+    elsif [404, 403, 401, 400].include?(response.status)
+      error = parse(response.body)
+      error_string = "#{error['code']} - #{error['error']} #{error['general_problem']} #{error['possible_fix']}"
+      raise AhaService::RemoteError, "Error code: #{error_string}"
+    else
+      raise AhaService::RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
+    end
+  end
+
+  def parse(body)
+    if body.nil? or body.length < 2
+      {}
+    else
+      JSON.parse(body)
+    end
+  end
+
 end
--- spec/services/redmine_spec.rb
+++ spec/services/redmine_spec.rb
@@ -7,6 +7,7 @@ describe AhaServices::Redmine do
     let(:schema_fields) {
       [
         [:string, :project_name, {}],
+        [:string, :redmine_url, {}],
         [:string, :api_key, {}]
       ]
     }
@@ -19,5 +20,25 @@ describe AhaServices::Redmine do
     it "has required schema fields" do
       expect(described_class.schema).to eq schema_fields
     end
+  end
+
+  context "installation" do
+    let(:service) { described_class.new redmine_url: 'http://localhost:4000', api_key: '123456' }
+    let(:raw_response) { raw_fixture('redmine/projects.json') }
+    let(:json_response) { JSON.parse(raw_response) }
+
+    before do
+      stub_request(:get, "#{service.data.redmine_url}/projects.json").
+        to_return(status: 200, body: raw_response, headers: {})
+    end
+
+    it "handles installed event" do
+      service.receive(:installed)
+      service.meta_data.projects.each_with_index do |proj, index|
+        proj["name"].should == json_response['projects'][index]['name']
+        proj["id"].should == json_response['projects'][index]['id']
+      end
+    end
+
   end
 end
