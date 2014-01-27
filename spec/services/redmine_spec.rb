4389221502b994a9d4f1d9f36e9b824b5aaa5dc1
ellmo <ellmunzai@gmail.com>
2014-01-27 13:03:21 +0100

    added spec context for project creation, when there are projects installed already

--- spec/services/redmine_spec.rb
+++ spec/services/redmine_spec.rb
@@ -75,12 +75,35 @@ describe AhaServices::Redmine do
         service.receive(:create_project)
       end
 
-      it "handles receive_create_project event" do
-        service.receive(:create_project)
-        new_project = service.meta_data.projects.last
-        expect(new_project[:name]).to eq project_name
-        expect(new_project[:id]).to eq json_response['project']['id']
+      context 'no projects previously installed' do
+        it "handles receive_create_project event" do
+          service.receive(:create_project)
+          new_project = service.meta_data.projects.last
+          expect(service.meta_data.projects.size).to eq 1
+          expect(new_project[:name]).to eq project_name
+          expect(new_project[:id]).to eq json_response['project']['id']
+        end
       end
+
+      context 'some projects already isntalled' do
+        let(:project_index_response_raw) { raw_fixture('redmine/projects.json') }
+        before do
+          stub_request(:get, "#{service.data.redmine_url}/projects.json").
+            to_return(status: 200, body: project_index_response_raw, headers: {})
+        end
+
+        it "handles receive_create_project event" do
+          service.receive(:installed)
+          old_project_count = service.meta_data.projects.count
+          service.receive(:create_project)
+          new_project = service.meta_data.projects.last
+          expect(service.meta_data.projects.size).to eq(1 + old_project_count)
+          expect(new_project[:name]).to eq project_name
+          expect(new_project[:id]).to eq json_response['project']['id']
+        end
+      end
+
+
     end
 
     context 'unauthenticated' do
