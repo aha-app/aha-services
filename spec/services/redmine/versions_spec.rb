658871844633e9676ef4ccb80ad28d5c3a022099
ellmo <ellmunzai@gmail.com>
2014-01-28 17:43:39 +0100

    create_version event

--- spec/services/redmine/versions_spec.rb
+++ spec/services/redmine/versions_spec.rb
@@ -7,7 +7,6 @@ describe AhaServices::Redmine do
     let(:projects_index_json) { JSON.parse projects_index_raw }
     let(:project_id) { projects_index_json['projects'].first['id'] }
     let(:version_name) { 'The Final Milestone' }
-    let(:version_identifier) { 'the-final-milestone' }
     let(:service) do
       described_class.new(
         { redmine_url: 'http://localhost:4000', api_key: '123456' },
@@ -19,12 +18,14 @@ describe AhaServices::Redmine do
     end
 
     context 'authenticated' do
-      # let(:raw_response) { raw_fixture('redmine/projects/create.json') }
-      # let(:json_response) { JSON.parse(raw_response) }
+      let(:raw_response) { raw_fixture('redmine/versions/create.json') }
+      let(:json_response) { JSON.parse(raw_response) }
 
       before do
+        stub_request(:get, "#{service.data.redmine_url}/projects.json").
+          to_return(status: 200, body: projects_index_raw, headers: {})
         stub_request(:post, "#{service.data.redmine_url}/projects/#{project_id}/versions.json").
-          to_return(status: 200, body: {}, headers: {})
+          to_return(status: 201, body: raw_response, headers: {})
       end
 
       it "responds to receive(:create_version)" do
@@ -33,12 +34,15 @@ describe AhaServices::Redmine do
       end
 
       context 'no other versions previously installed' do
+        let(:project) { service.meta_data.projects.find {|p| p[:id] == project_id }}
+        let(:new_version) { project[:versions].last }
+
         it "handles receive_create_version event" do
           service.receive(:create_version)
-          new_version = service.meta_data.versions.last
-          expect(service.meta_data.versions.size).to eq 1
-          expect(new_version[:name]).to eq project_name
-          # expect(new_version[:id]).to eq json_response['project']['id']
+
+          expect(project[:versions].size).to eq 1
+          expect(new_version[:name]).to eq version_name
+          expect(new_version[:id]).to eq json_response['version']['id']
         end
       end
 
