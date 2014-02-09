501e05e3d0f91c5741ebc9aa7ebfc6b79632c4de
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-02-09 23:31:42 +0000

    Remove unused code

--- lib/services/jira.rb
+++ lib/services/jira.rb
@@ -273,38 +273,6 @@ protected
     end
   end
   
-  def parse(body)
-    if body.nil? or body.length < 2
-      {}
-    else
-      JSON.parse(body)
-    end
-  end
-  
-  def prepare_request
-    http.headers['Content-Type'] = 'application/json'
-    auth_header
-  end
-  
-  def auth_header
-    http.basic_auth data.username, data.password
-  end
-  
-  def process_response(response, *success_codes, &block)
-    if success_codes.include?(response.status)
-      yield parse(response.body)
-    elsif response.status == 401 || response.status == 403
-      raise AhaService::RemoteError, "Authentication failed: #{response.status} #{response.headers['X-Authentication-Denied-Reason']}"
-    elsif response.status == 400
-      errors = parse(response.body)
-      error_string = errors["errorMessages"].join(", ") + 
-        errors["errors"].map {|k, v| "#{k}: #{v}" }.join(", ")
-      raise AhaService::RemoteError, "Data not accepted: #{error_string}"
-    else
-      raise AhaService::RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
-    end
-  end
-  
   def issue_type(issue_type_id)
     raise AhaService::RemoteError, "Integration has not been configured" if @meta_data.projects.nil?
     project = @meta_data.projects.find {|project| project['key'] == data.project }
