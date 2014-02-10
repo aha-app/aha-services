ea52c12a1e531c01b6f431967525879a05722b01
ellmo <ellmunzai@gmail.com>
2014-02-10 18:36:23 +0100

    moving redmine project installation to redmine_resource

---
+++ lib/services/redmine/redmine_resource.rb
@@ -0,0 +1,49 @@
+class RedmineResource
+  include Networking
+  include Errors
+
+  attr_reader :logger
+
+  def initialize(service)
+    @service = service
+    @logger = service.data.logger || allocate_logger
+  end
+
+  def parse(body)
+    if body.nil? or body.length < 2
+      {}
+    else
+      JSON.parse(body)
+    end
+  end
+
+  def prepare_request
+    http.headers['Content-Type'] = 'application/json'
+    http.headers['X-Redmine-API-Key'] = @service.data.api_key
+  end
+
+  def process_response(response, *success_codes, &block)
+    if success_codes.include?(response.status)
+      yield parse(response.body)
+    elsif response.status.between?(400, 499)
+      error = parse(response.body)
+      raise RemoteError, "Error message: #{error['message']}"
+    else
+      raise RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
+    end
+  end
+
+  def self.default_http_options
+    @@default_http_options ||= {
+      :request => {:timeout => 310, :open_timeout => 5},
+      :ssl => {:verify => false, :verify_depth => 5},
+      :headers => {}
+    }
+  end
+
+  def allocate_logger
+    @logger = AhaLogger.new(STDOUT)
+    @logger.level = Logger::DEBUG
+    @logger
+  end
+end
