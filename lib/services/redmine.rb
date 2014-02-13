d5b0d975f30397ad1dce6d133fc339e5b1705e62
ellmo <ellmunzai@gmail.com>
2014-02-13 14:18:34 +0100

    removing unused pieces of code, which was moved to resource classes

--- lib/services/redmine.rb
+++ lib/services/redmine.rb
@@ -49,29 +49,22 @@ private
   end
 
   def create_version
-    @meta_data.projects ||= []
-    install_projects if @meta_data.projects.empty?
+
     version_resource.create
   end
 
   def create_issue payload_fragment=nil, parent_id=nil
-    @meta_data.projects ||= []
-    unless payload_fragment && parent_id
-      issue_resource.create
-    else
-      issue_resource.create payload_fragment, parent_id
-    end
+    check_projects
+    issue_resource.create payload_fragment, parent_id
   end
 
   def update_version
-    @meta_data.projects ||= []
-    install_projects if @meta_data.projects.empty?
+    check_projects
     version_resource.update
   end
 
   def update_issue
-    @meta_data.projects ||= []
-    install_projects if @meta_data.projects.empty?
+    check_projects
     issue_resource.update
   end
 
@@ -91,44 +84,14 @@ private
     @issue_resource ||= RedmineIssueResource.new(self)
   end
 
-#==================
-# REQUEST HANDLING
-#================
-
-  def prepare_request
-    http.headers['Content-Type'] = 'application/json'
-    http.headers['X-Redmine-API-Key'] = data.api_key
-  end
-
-  def process_response(response, *success_codes, &block)
-    if success_codes.include?(response.status)
-      yield parse(response.body)
-    elsif [404, 403, 401, 400].include?(response.status)
-      error = parse(response.body)
-      error_string = "#{error['code']} - #{error['error']} #{error['general_problem']} #{error['possible_fix']}"
-      raise AhaService::RemoteError, "Error code: #{error_string}"
-    else
-      raise AhaService::RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
-    end
-  end
-
-  def parse(body)
-    if body.nil? or body.length < 2
-      {}
-    else
-      Hashie::Mash.new JSON.parse(body)
-    end
-  end
-
-  def create_integrations reference, **fields
-    fields.each do |field, value|
-      api.create_integration_field(reference, self.class.service_name, field, value)
-    end
-  end
-
 #=========
 # SUPPORT
 #=======
+
+  def check_projects
+    @meta_data.projects ||= []
+    install_projects if @meta_data.projects.empty?
+  end
 
   def find_project project_id
     @meta_data.projects.find {|p| p[:id] == project_id }
