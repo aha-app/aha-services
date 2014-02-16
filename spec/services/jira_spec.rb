6b190b68bffb78aab8b55844f9492d34bb57439e
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-02-17 02:09:11 +0400

    Add specs for version_fields method

--- spec/services/jira_spec.rb
+++ spec/services/jira_spec.rb
@@ -227,6 +227,23 @@ describe AhaServices::Jira do
     end
   end
 
+  describe "#version_fields" do
+    context "when a version exists" do
+      it "returns a specific hash" do
+        version = Hashie::Mash.new(id: '1001')
+        expect(service.send(:version_fields, version))
+          .to eq(fixVersions: [{ id: version.id }])
+      end
+    end
+
+    context "when there is no version" do
+      it "returns an empty hash" do
+        expect(service.send(:version_fields, nil))
+          .to eq Hash.new
+      end
+    end
+  end
+
   describe "#label_fields" do
     shared_examples "empty label fields" do
       it "returns an empty hash" do
