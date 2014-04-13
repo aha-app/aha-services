5dfa8a7030c862d60c190d21243b4db524e4705f
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-04-13 13:23:44 +0400

    Edit specs and add a 'receive_update_feature' spec

--- spec/services/trello_spec.rb
+++ spec/services/trello_spec.rb
@@ -3,15 +3,20 @@ require "spec_helper"
 describe AhaServices::Trello do
   let(:base_url) { "https://api.trello.com/1" }
   let(:service) do
-    AhaServices::Trello.new 'server_url' => base_url,
-                            'key' => key, 'secret' => secret
+    AhaServices::Trello.new 'server_url' => base_url
   end
 
   it "can receive new features" do
-    service.stub(:payload).and_return(Hashie::Mash.new(json_fixture("create_feature_event.json")))
-    stub_request(:post, "#{base_url}/cards")
-      .to_return(status: 201, body: '{"id": "12345"}')
+    service.stub(:payload)
+      .and_return(Hashie::Mash.new(json_fixture("create_feature_event.json")))
     service.should_receive(:create_or_update_trello_card)
     service.receive(:create_feature)
+  end
+
+  it "can update existing features" do
+    service.stub(:payload)
+      .and_return(Hashie::Mash.new(json_fixture("update_feature_event.json")))
+    service.should_receive(:create_or_update_trello_card)
+    service.receive(:update_feature)
   end
 end
