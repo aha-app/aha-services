2e4e678abbb187a6381300b9d30a7df09233c03c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-04-26 10:55:16 +0200

    Create and update tasks, add attachments to corresponding stories

--- lib/services/pivotal_tracker/pivotal_tracker_story_resource.rb
+++ lib/services/pivotal_tracker/pivotal_tracker_story_resource.rb
@@ -15,6 +15,23 @@ class PivotalTrackerStoryResource < PivotalTrackerProjectDependentResource
     update_from_resource(requirement_mapping, requirement, feature_mapping)
   end
 
+  def add_attachments(story_id, new_attachments)
+    if new_attachments.any?
+      response = http_post("#{api_url}/projects/#{project_id}/stories/#{story_id}/comments", {file_attachments: new_attachments}.to_json)
+      process_response(response, 200) do |updated_story|
+        logger.info("Updated story #{story_id}")
+      end
+    end
+  end
+
+  def update(story_id, story)
+    prepare_request
+    response = http_put("#{api_url}/projects/#{project_id}/stories/#{story_id}", story.to_json)
+    process_response(response, 200) do |updated_story|
+      logger.info("Updated story #{story_id}")
+    end
+  end
+
 protected
 
   def create(story)
@@ -24,14 +41,6 @@ protected
     process_response(response, 200) do |created_story|
       logger.info("Created story #{created_story.id}")
       return created_story
-    end
-  end
-
-  def update(story_id, story)
-    prepare_request
-    response = http_put("#{api_url}/projects/#{project_id}/stories/#{story_id}", story.to_json)
-    process_response(response, 200) do |updated_story|
-      logger.info("Updated story #{story_id}")
     end
   end
 
--- lib/services/pivotal_tracker/pivotal_tracker_task_resource.rb
+++ lib/services/pivotal_tracker/pivotal_tracker_task_resource.rb
@@ -3,13 +3,13 @@ class PivotalTrackerTaskResource < PivotalTrackerProjectDependentResource
     task = {
       description: task_description(requirement, feature_mapping.id),
       created_at: resource.created_at,
-      story_id: feature_mapping.id
     }
+    file_attachments = attachment_resource.upload(requirement.description.attachments | requirement.attachments)
     if file_attachments.any?
-      # story[:comments] = [{file_attachments: file_attachments}]
+      story_resource.update(feature_mapping.id, { comments: [ { file_attachments: file_attachments } ] })
     end
 
-    created_task = create(task)
+    created_task = create(task, feature_mapping.id)
     api.create_integration_fields(reference_num_to_resource_type(requirement.reference_num), requirement.reference_num, @service.class.service_name, {id: created_task.id})
     created_task
   end
@@ -19,45 +19,40 @@ class PivotalTrackerTaskResource < PivotalTrackerProjectDependentResource
       description: task_description(requirement, feature_mapping.id)
     }
 
-    update(requirement_mapping.id, task)
+    update(requirement_mapping.id, task, feature_mapping.id)
 
     # Add the new attachments.
-    # new_attachments = attachment_resource.update(resource, attachment_resource.all_for_story(resource_mapping.id))
-    # add_attachments(resource_mapping.id, new_attachments)
+    new_attachments = attachment_resource.update(requirement, attachment_resource.all_for_story(feature_mapping.id))
+    story_resource.add_attachments(feature_mapping.id, new_attachments)
   end
 
 protected
 
-  # def create(story)
-  #   prepare_request
-  #   response = http_post("#{api_url}/projects/#{project_id}/stories", story.to_json)
+  def story_resource
+    @story_resource ||= PivotalTrackerStoryResource.new(@service, project_id)
+  end
 
-  #   process_response(response, 200) do |created_story|
-  #     logger.info("Created story #{created_story.id}")
-  #     return created_story
-  #   end
-  # end
+  def attachment_resource
+    @attachment_resource ||= PivotalTrackerAttachmentResource.new(@service, project_id)
+  end
 
-  # def update(story_id, story)
-  #   prepare_request
-  #   response = http_put("#{api_url}/projects/#{project_id}/stories/#{story_id}", story.to_json)
-  #   process_response(response, 200) do |updated_story|
-  #     logger.info("Updated story #{story_id}")
-  #   end
-  # end
+  def create(task, story_id)
+    prepare_request
+    response = http_post("#{api_url}/projects/#{project_id}/stories/#{story_id}/tasks", task.to_json)
 
-  # def add_attachments(story_id, new_attachments)
-  #   if new_attachments.any?
-  #     response = http_post("#{api_url}/projects/#{project_id}/stories/#{story_id}/comments", {file_attachments: new_attachments}.to_json)
-  #     process_response(response, 200) do |updated_story|
-  #       logger.info("Updated story #{story_id}")
-  #     end
-  #   end
-  # end
+    process_response(response, 200) do |created_task|
+      logger.info("Created task #{created_task.id}")
+      return created_task
+    end
+  end
 
-  # def attachment_resource
-  #   @attachment_resource ||= PivotalTrackerAttachmentResource.new(@service, project_id)
-  # end
+  def update(task_id, task, story_id)
+    prepare_request
+    response = http_put("#{api_url}/projects/#{project_id}/stories/#{story_id}/tasks/#{task_id}", task.to_json)
+    process_response(response, 200) do |updated_task|
+      logger.info("Updated task #{task_id}")
+    end
+  end
 
   def task_description(requirement, feature_mapping_id)
     [requirement.name, append_link(html_to_plain(requirement.description.body), feature_mapping_id)].join("\n")
