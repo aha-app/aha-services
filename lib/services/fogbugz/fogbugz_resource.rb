ccaeccfb0c31870d8f90e02dc457732811e5ec85
Jeremy Geros <jeremy453@gmail.com>
2014-08-12 11:28:26 -0500

    Only encode form if not attachments

--- lib/services/fogbugz/fogbugz_resource.rb
+++ lib/services/fogbugz/fogbugz_resource.rb
@@ -29,8 +29,11 @@ class FogbugzResource < GenericResource
     end
 
     params[:sEvent] = Sanitize.fragment(params[:sEvent]).strip if params[:sEvent]
+    params = params.merge!({'token' => @token, 'cols' => request_columns})
+    
+    params = URI.encode_www_form(params) if attachments.empty?
 
-    response = http_post("#{ api_url }#{ command }", URI.encode_www_form(params.merge({'token' => @token, 'cols' => request_columns})))
+    response = http_post("#{ api_url }#{ command }", params)
     process_response(response, 200).response
   end
 
