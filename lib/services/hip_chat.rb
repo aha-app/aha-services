bea69363ce66ddd0cbb9d9a08dc21c536d2475e4
k1w1 <k1w1@k1w1.org>
2014-08-22 15:25:06 -0700

    * Hipchat integration is working.

--- lib/services/hip_chat.rb
+++ lib/services/hip_chat.rb
@@ -1,13 +1,31 @@
 class AhaServices::HipChat < AhaService
+  caption "Send all activity into a group chat for teams"
+  
   string :auth_token,
     description: "An authentication token from HipChat. For best security you should use a 'Room Notification Token'."
   string :room_name, description: "The name or API ID of the room messages should be sent to."
   install_button
   
   def receive_installed
-    send_message("Test message from <b>Aha!</b>")
+    send_message("Test message from <a href='http://www.aha.io/'>Aha!</a>")
   end
   
+  def receive_audit
+    audit = payload.audit
+    user = if audit.user
+        audit.user.name
+      else
+        "Aha!"
+      end
+
+    fields = audit.changes.collect { |change| "<b>#{change.field_name}</b> #{change.value}<br/>" }
+    
+    send_message <<-EOS
+      #{user} <a href="#{audit.auditable_url}">#{audit.description}</a><br/>
+      #{fields.join("\n")}
+    EOS
+  end
+    
 protected
 
   def send_message(message)
