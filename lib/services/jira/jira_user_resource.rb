37d4c2dd87413b5ee4720ff0bd73f6dd421141ca
k1w1 <k1w1@k1w1.org>
2014-09-03 13:24:18 -0700

    * Handle 404 error that is returned by older versions of JIRA when a user is not found. Newer versions return an empty list.

--- lib/services/jira/jira_user_resource.rb
+++ lib/services/jira/jira_user_resource.rb
@@ -2,6 +2,9 @@ class JiraUserResource < JiraResource
   def picker(email)
     prepare_request
     response = http_get "#{api_url}/user/picker?query=#{email}"
+    if response.status == 404
+      return nil
+    end
     process_response(response, 200) do |response|
       return response.users.first if response.users && response.users.any?
       nil
