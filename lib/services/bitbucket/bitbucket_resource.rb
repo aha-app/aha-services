53250f701e5a57316f67e00ccbe4bcd0b48982c7
Josh Robinson <hexorx@gmail.com>
2014-12-23 17:10:06 -0700

    Updated bitbucket error messages

--- lib/services/bitbucket/bitbucket_resource.rb
+++ lib/services/bitbucket/bitbucket_resource.rb
@@ -21,9 +21,15 @@ class BitbucketResource < GenericResource
   def process_response(response, *success_codes, &block)
     if success_codes.include?(response.status)
       yield parse(response.body)
+    elsif reponse.status == 401
+      raise RemoteError, "Credentials are incorrect."
+    elsif reponse.status == 403
+      raise RemoteError, "You do not have permissions to access this resource."
+    elsif reponse.status == 404
+      raise RemoteError, "Resource does not exist."
     elsif response.status.between?(400, 499)
       error = parse(response.body)
-      raise RemoteError, "Error message: #{error['message']}"
+      raise RemoteError, "#{error['message']}"
     else
       raise RemoteError, "Unhandled error: STATUS=#{response.status} BODY=#{response.body}"
     end
