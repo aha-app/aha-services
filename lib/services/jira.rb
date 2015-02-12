999a846ee17329b824379b1bde0658a56d728b3f
Felix Maurer <Felix.Maurer@gmx.net>
2015-02-12 14:50:40 +0100

    Enable resolution mapping field

--- lib/services/jira.rb
+++ lib/services/jira.rb
@@ -8,19 +8,21 @@ class AhaServices::Jira < AhaService
   string :username, description: "Use your JIRA username from the JIRA profile page, not your email address."
   password :password
   install_button
-  select :project, collection: ->(meta_data, data) { meta_data.projects.collect{|p| [p.name, p[:key]] } }
+  select :project, collection: ->(meta_data, data) { meta_data.projects.collect{|p| [p.name, p[:key]] } rescue [] }
   boolean :send_initiatives, description: "Check to use feature initatives to create Epics in JIRA Agile"
   select :feature_issue_type, 
     collection: ->(meta_data, data) { 
-      meta_data.issue_type_sets[meta_data.projects.detect {|p| p[:key] == data.project}.issue_types].find_all{|i| !i.subtype}.collect{|p| [p.name, p.id] }
+      meta_data.issue_type_sets[meta_data.projects.detect{|p| p[:key] == data.project }.issue_types]
+      .find_all{|i| !i.subtype}.collect{|p| [p.name, p.id] } rescue []
     }, description: "JIRA issue type that will be used when sending features. If you are using JIRA Agile then we recommend 'Story'."
   select :requirement_issue_type, 
     collection: ->(meta_data, data) { 
-      meta_data.issue_type_sets[meta_data.projects.detect {|p| p[:key] == data.project}.issue_types].find_all{|i| !i.subtype}.collect{|p| [p.name, p.id] }
+      meta_data.issue_type_sets[meta_data.projects.detect {|p| p[:key] == data.project}.issue_types]
+      .find_all{|i| !i.subtype}.collect{|p| [p.name, p.id] } rescue []
     }, description: "JIRA issue type that will be used when sending requirements. If you are using JIRA Agile then we recommend 'Sub-task'."
   internal :feature_status_mapping
   internal :field_mapping
-  # internal :resolution_mapping  # TODO: we are not actually using this at the moment.
+  internal :resolution_mapping
   boolean :dont_send_releases, description: "Check to prevent Aha! from creating versions in JIRA and from populating the fixVersions field for issues. For most users this box should not be checked."
   boolean :send_tags, description: "Check to synchronize Aha! tags and JIRA labels. We recommend enabling this for new integrations. Enabling this option once features are synced to JIRA may cause tags in Aha! or labels in JIRA to be removed from a feature if the corresponding label or tag doesn't exist in the other system."
   
