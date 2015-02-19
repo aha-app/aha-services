a1125d4767845207d1c19b61d4dc39ef8378e041
k1w1 <k1w1@k1w1.org>
2015-02-19 09:34:15 -0800

    Use a larger default page size when fetching Rally projects.

--- lib/aha-services/version.rb
+++ lib/aha-services/version.rb
@@ -1,3 +1,3 @@
 module AhaServices
-  VERSION = "1.16.5"
+  VERSION = "1.16.6"
 end
--- lib/services/rally/rally_project_resource.rb
+++ lib/services/rally/rally_project_resource.rb
@@ -1,6 +1,6 @@
 class RallyProjectResource < RallyResource
   def all
-    url = rally_url "/project?fetch=true"
+    url = rally_url "/project?fetch=true&pagesize=100"
     response = http_get url
     process_response response do |document|
       return document.QueryResult.Results.map{|project| project.slice("ObjectID", "_ref", "Name") }
