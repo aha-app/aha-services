7660eb97d8b14bfda67f3a795062fb11e77a448f
k1w1 <chris@aha.io>
2015-02-25 10:35:20 -0800

    Fetch all projects, order by name.

--- lib/aha-services/version.rb
+++ lib/aha-services/version.rb
@@ -1,3 +1,3 @@
 module AhaServices
-  VERSION = "1.16.6"
+  VERSION = "1.16.7"
 end
--- lib/services/rally/rally_project_resource.rb
+++ lib/services/rally/rally_project_resource.rb
@@ -1,9 +1,18 @@
 class RallyProjectResource < RallyResource
   def all
-    url = rally_url "/project?fetch=true&pagesize=100"
-    response = http_get url
-    process_response response do |document|
-      return document.QueryResult.Results.map{|project| project.slice("ObjectID", "_ref", "Name") }
+    projects = []
+    start = 1
+    total_results = 0
+    loop do 
+      url = rally_url "/project?fetch=true&pagesize=100&start=#{start}"
+      response = http_get url
+      process_response response do |document|
+        total_results = document.QueryResult.TotalResultCount
+        start += document.QueryResult.PageSize
+        projects.concat(document.QueryResult.Results.map{|project| project.slice("ObjectID", "_ref", "Name") })
+      end
+      break if start >= total_results
     end
+    projects.sort_by {|r| r["Name"] }
   end
 end
