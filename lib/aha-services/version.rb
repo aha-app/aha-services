cfc9e3502a3f908d34bac375599b3139917fc723
Chris Waters <k1w1@k1w1.org>
2015-05-18 08:39:00 -0700

    Fetch more redmine projects. The default is 25.

--- lib/aha-services/version.rb
+++ lib/aha-services/version.rb
@@ -1,3 +1,3 @@
 module AhaServices
-  VERSION = "1.17.10"
+  VERSION = "1.17.11"
 end
--- lib/services/redmine/redmine_project_resource.rb
+++ lib/services/redmine/redmine_project_resource.rb
@@ -1,7 +1,7 @@
 class RedmineProjectResource < RedmineResource
   def all
     prepare_request
-    response = http_get "#{@service.data.redmine_url}/projects.json"
+    response = http_get "#{@service.data.redmine_url}/projects.json?limit=200"
     process_response response, 200 do |body|
       @projects = body['projects']
     end
