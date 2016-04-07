2edeb388472716118ac3332d2101df0dc16ad1ff
Alex Bartlow <bartlowa@gmail.com>
2016-04-07 11:45:50 -0400

    Use Ref instead of ID

--- lib/services/rally/rallly_user_resource.rb
+++ lib/services/rally/rallly_user_resource.rb
@@ -2,7 +2,8 @@ class RallyUserResource < RallyResource
   def user_id_for_email(email)
     url = rally_url_without_workspace("/user?" + {query: "(EmailAddress = #{email})"}.to_query)
     process_response http_get(url) do |document|
-      return document.QueryResult.Results.first.try(&:ObjectID)
+      user = document.QueryResult.Results.first
+      return user["_ref"] if user
     end
   end
 end
