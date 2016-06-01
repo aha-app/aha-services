0d8d5bddd749b3100a765a58f3844f6d6c5c8509
Alex Bartlow <bartlowa@gmail.com>
2016-06-01 10:18:48 -0400

    JIRA Install button should no longer forget about already fetched issue_type_sets

--- lib/services/jira.rb
+++ lib/services/jira.rb
@@ -126,7 +126,14 @@ protected
     @meta_data['story_points_field'] = field_resource.story_points_field
     @meta_data['aha_position_field'] = field_resource.aha_position_field
     @meta_data['aha_reference_field'] = new_or_existing_aha_reference_field
+    old_projects = Hash[@meta_data.fetch("projects", []).map {|project| [project["id"].to_s, project] }]
     @meta_data["projects"] = project_resource.list
+    @meta_data["projects"].each do |project|
+      # since issue types is fetched in another step, don't lose that configuration data when re-installing
+      if issue_types = old_projects.fetch(project["id"].to_s, {})["issue_types"]
+        project["issue_types"] = issue_types
+      end
+    end
     @meta_data['resolutions'] = resolution_resource.all
   end
   
