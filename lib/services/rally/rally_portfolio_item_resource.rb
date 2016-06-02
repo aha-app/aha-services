831316b206387ae827bc50d20fc3614e9c5d4ee5
Dave Rapin <dave@rapin.com>
2016-06-02 16:09:57 -0400

    Augmented the portfolio items response to merge in a custom fields array

--- lib/services/rally/rally_portfolio_item_resource.rb
+++ lib/services/rally/rally_portfolio_item_resource.rb
@@ -1,4 +1,5 @@
 class RallyPortfolioItemResource < RallyResource
+
   def get_all_portfolio_items
     path = "/typedefinitions?" + {
       start: 1,
@@ -7,8 +8,33 @@ class RallyPortfolioItemResource < RallyResource
       fetch: "true"
     }.to_query
 
+    # Return our type definitions including their custom fields if they have any
     process_response(http_get(rally_secure_url(path))) do |response|
-      return response.QueryResult.Results.sort_by{ |result| result.Ordinal } # Lowest Ordinal => Lowest unit on the heirarchy.
+      response.QueryResult.Results.sort_by do |type_definition|
+        type_definition.Ordinal # Lowest Ordinal => Lowest unit on the heirarchy.
+      end.map do |type_definition|
+        # Add the definitions custom fields
+        custom_fields = custom_fields_for_type_definition(type_definition)
+        type_definition["CustomFields"] = custom_fields
+        type_definition
+      end
+    end
+  end
+
+  private
+
+  def custom_fields_for_type_definition(definition)
+    # We need to crawl each type definition to get the potential custom fields
+    url = "#{definition["Attributes"]["_ref"]}?" + {
+      start: 1,
+      pagesize: 200 # TODO - try to filter the query with "Custom = true"
+    }.to_query
+
+    process_response(http_get(url)) do |response|
+      # We only want the custom fields, so filter on the Custom attribute.
+      response["QueryResult"]["Results"].select { |attr| attr["Custom"] }.map do |attr|
+        attr.slice("AttributeType", "Name", "ElementName")
+      end
     end
   end
 end
