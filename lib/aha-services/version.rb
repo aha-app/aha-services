16c61c857366ab29555a2d453325555a095137f6
Alex Bartlow <bartlowa@gmail.com>
2016-09-23 10:26:25 -0400

    Search for users in Rally using both the EmailAddress and UserName field, to handle case sensitive emails

--- lib/aha-services/version.rb
+++ lib/aha-services/version.rb
@@ -1,3 +1,3 @@
 module AhaServices
-  VERSION = "1.23.4"
+  VERSION = "1.23.5"
 end
--- lib/services/rally/rally_user_resource.rb
+++ lib/services/rally/rally_user_resource.rb
@@ -1,6 +1,6 @@
 class RallyUserResource < RallyResource
   def user_id_for_email(email)
-    url = rally_url_without_workspace("/user?" + {query: "(EmailAddress = #{email})"}.to_query)
+    url = rally_url_without_workspace("/user?" + {query: "((EmailAddress = #{email}) OR (UserName = #{email}))"}.to_query)
     process_response http_get(url) do |document|
       user = document.QueryResult.Results.first
       return user["_ref"] if user
