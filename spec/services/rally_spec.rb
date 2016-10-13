6cbdbf28c409d825885e8966705953643103a7f2
Peter Klipfel <peter@klipfel.me>
2016-10-13 15:43:12 -0600

    started tests for rally

--- spec/services/rally_spec.rb
+++ spec/services/rally_spec.rb
@@ -14,8 +14,23 @@ describe AhaServices::Rally do
     # stub_request(:get, //).
     #   to_return(status: 200, body: raw_fixture("rally/tbd.json"))
   end
+  let(:service_params) do
+    {}
+  end
+
+  let (:service) do
+    AhaServices::Rally.new service_params
+  end
+
+  let(:h_req_service) do
+    RallyHierarchicalRequirementResource.new service
+  end
 
   it "can be installed"
+  it "creates the right tag queries" do
+    expect(h_req_service.send( :build_tag_query, ["1", "2", "3", "4", "5", "6"] ))
+      .to eq '((((((Name = "1") OR (Name = "2")) OR (Name = "3")) OR (Name = "4")) OR (Name = "5")) OR (Name = "6"))'
+  end
 
   context "project" do
     it "can be updated"
