a76e982d671705335722e1841d497a596a928f23
pwaller24 <pwaller@p2energysolutions.com>
2017-05-04 09:44:45 -1000

    debugging message

--- lib/services/p2pm/p2pm_project_resource.rb
+++ lib/services/p2pm/p2pm_project_resource.rb
@@ -7,7 +7,7 @@ class P2PMProjectResource < P2PMResource
     body = '{"client_id":"ORFAVREOUWRAUGGRQJGTNKDRHKBSETWT","client_secret":"434157704590a695188bf57026369405","username":"admin","password":"admin","grant_type":"password"}'
     puts body
     puts body.to_json
-    puts @service.data_server_url
+    puts @service.data.server_url
     response = http_post @service.data.server_url do |req|
       req.headers['Content-Type'] = 'application/json'
       req.headers['Cache-Control'] = 'no-cache'
