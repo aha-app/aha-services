94fc6906819f3d15cf8bc863d8a17ab1045716f4
Pete Waller <pwaller@p2energysolutions.com>
2017-05-11 11:09:57 -1000

    atypo

--- lib/services/p2pm/p2pm_project_resource.rb
+++ lib/services/p2pm/p2pm_project_resource.rb
@@ -42,7 +42,7 @@ class P2PMProjectResource < P2PMResource
       parsed = JSON.parse(body)
       puts parsed
       parsed.each do |table|
-        tables[table["pmt_uid"]] = Hashie::Mash.new({:id => table{"pmt_uid"], :name => table.["pmt_tab_name"]})
+        tables[table["pmt_uid"]] = Hashie::Mash.new({:id => table["pmt_uid"], :name => table.["pmt_tab_name"]})
       end
       tables
     end
