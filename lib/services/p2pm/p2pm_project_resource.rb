a7b95a3007c117a9c2ddfbdcbd7cbbb6b88f6a16
Pete Waller <pwaller@p2energysolutions.com>
2017-05-10 15:06:43 -1000

    displaying response

--- lib/services/p2pm/p2pm_project_resource.rb
+++ lib/services/p2pm/p2pm_project_resource.rb
@@ -14,6 +14,7 @@ class P2PMProjectResource < P2PMResource
     }
     
     response = RestClient.post @service.data.server_url, body.to_json, {content_type: :json, accept: :json}
+    puts reponse
     #response = http_post @service.data.server_url, body.to_json
     process_response response do |document|
       self.security_token = document.OperationResult.SecurityToken
