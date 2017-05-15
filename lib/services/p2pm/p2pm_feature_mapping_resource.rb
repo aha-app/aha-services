fd5b9cc04d71134ae234768a9f1cc777554c9be6
pwaller24 <pwaller@p2energysolutions.com>
2017-05-15 13:35:27 -1000

    figuring how to pull data from the aha_feature

--- lib/services/p2pm/p2pm_feature_mapping_resource.rb
+++ lib/services/p2pm/p2pm_feature_mapping_resource.rb
@@ -7,7 +7,7 @@ class P2PMFeatureMappingResource < P2PMResource
       "System.AreaPath" => @service.data.area
     }
     puts aha_feature
-    puts aha_feature.custom_fields.customer
+    puts aha_feature.custom_fields['customer'].value
 
     body = {
       "REPRO_STEPS" => aha_feature.custom_fields.bug_repro_steps,
