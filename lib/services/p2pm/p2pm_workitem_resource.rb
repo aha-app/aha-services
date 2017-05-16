b09ba1838119cc0abd2adf57becd99d21b807fe5
pwaller24 <pwaller@p2energysolutions.com>
2017-05-16 13:20:24 -1000

    forgot to save

--- lib/services/p2pm/p2pm_workitem_resource.rb
+++ lib/services/p2pm/p2pm_workitem_resource.rb
@@ -10,7 +10,7 @@ class P2PMWorkItemResource < P2PMResource
   end
 
   # Get the ProcessMaker TFS_DATA record by the ID.
-  def by_id id, table
+  def by_id id, table, sec_token
     http.headers["Authorization"] = "Bearer " + sec_token
     url = @service.data.data_url + "/api/1.0/workflow/pmtable/"+ table + '/data?q={"where": {"ID": ' + id+ '}}'
     by_url url
