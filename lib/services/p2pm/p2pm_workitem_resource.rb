057a33914706a2f92e1a9d5f78b95edf40123a65
pwaller24 <pwaller@p2energysolutions.com>
2017-06-12 06:03:47 -1000

    requirements is an array

--- lib/services/p2pm/p2pm_workitem_resource.rb
+++ lib/services/p2pm/p2pm_workitem_resource.rb
@@ -64,7 +64,7 @@ class P2PMWorkItemResource < P2PMResource
 			    "reproSteps" => get_custom_field_value(aha_feature,"bug_repro_steps"),
 			    "salesforceId" => get_custom_field_value(aha_feature,"salesforce_id"),
 			    "title" => aha_feature.name,
-          "requirements" => aha_feature.requirements.body,
+          "requirements" => aha_feature.requirements[0].body,
           "description" => aha_feature.description,
 			    "type" => aha_feature.workflow_kind.name
 		    }
