b67a873edf61174bc1e2067ca0bf2167c75f20c5
pwaller24 <pwaller@p2energysolutions.com>
2017-06-21 13:37:49 -1000

    And another try

--- lib/services/p2pm/p2pm_workitem_resource.rb
+++ lib/services/p2pm/p2pm_workitem_resource.rb
@@ -218,9 +218,11 @@ protected
       logger.debug "\nparsed: #{parsed}\n"
       logger.debug "\ncustom_fields: #{parsed['goal']['custom_fields']}\n"
       tfs_id = parsed['goal']['custom_fields'].find {|field| field['key'] == "tfs_id"}
-      logger.debug "\ntfs_id: #{tfs_id}"
-      #tfs_id = get_custom_field_value(parsed, "tfs_id")
-      tfs_id
+      if tfs_id
+        tfs_id.value
+      else
+        nil
+      end
     end
   end
 
