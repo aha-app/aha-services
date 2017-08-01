ca7f80ae521fcf6da718a4d5d11d80c7778cfa08
Jonathan Steel <jonathansteel128@hotmail.com>
2017-08-01 09:42:26 -0400

    Use a oauth and install buttons for Desk.com

--- lib/services/desk_dot_com.rb
+++ lib/services/desk_dot_com.rb
@@ -1,38 +1,87 @@
+require 'desk_api'
+
 class AhaServices::DeskDotCom < AhaService
   title "Desk.com"
   caption "Receive ideas from a Desk.com helpdesk"
-
-  # oauth_button request_token_url: "https://ahatest123.desk.com/oauth/request_token",
-  #   access_token_url: "https://ahatest123.desk.com/oauth/access_token",
-  #   authorize_url: "https://ahatest123.desk.com/oauth/authorize",
-  #   parameters: "name=Aha!%20Integration&scope=read,write&expiration=never"
-
-  string :desk_api_key,
-    label: "API Key",
-    description: "Your Desk.com API key"
-  string :desk_api_secret,
-    label: "API Secret",
-    description: "Your Desk.com API secret"
-  string :desk_token,
-    label: "API Token",
-    description: "Your Desk.com API access token"
-  string :desk_token_secret,
-    label: "API Token Secret",
-    description: "Your Desk.com API access token secret"
-
-  # install_button
-
-  string :shared_key, description: "Your Desk.com shared key"
 
   internal :idea_portal_id
 
   boolean :use_customer_name,
     description: "Use the customer name and email instead of the agents when creating a ticket"
 
+  string :oauth_host,
+    label: "Desk Host",
+    description: "Your Desk.com domain. For example: mydeskdomain.desk.com."
+
+  oauth_button request_token_path: "/oauth/request_token",
+    access_token_path: "/oauth/access_token",
+    authorize_path: "/oauth/authorize",
+    parameters: "name=Aha!%20Integration&scope=read,write&expiration=never",
+    callback_token_type: :access_token
+
+  install_button hide_fetch_message: true, label: "Create Integration URL"
+
+  internal :aha_integration_url
+
+  string :shared_key, description: "Your Desk.com shared key"
+
+  INTEGRATION_URL_NAME = "Aha"
+
   # TODO When we convert to integrations 2, add a field to be able to change
   # the callback user just like we have on the callback_url field
 
+  def receive_installed
+    unless find_canvas_integration_url
+      begin
+        # Warning, you will likely get a validation error in development here
+        # because of the http instead of https in the integration url.
+        response = client.integration_urls.create(
+          name: "Aha Ideas",
+          description: "Allows easy linking of cases to Aha ideas",
+          enabled: true,
+          markup: data.integration_url,
+          open_location: 'iframe_canvas')
+      rescue DeskApi::Error::InternalServerError => e
+        raise AhaService::RemoteError, "Integration has not been configured"
+        logger.error("There was an error creating the integration. You may not be allowed to create any more integration URLs.")
+        return false
+      end
+    end
+
+    return true
+  end
+
   def receive_updated
     return true
+  end
+
+  private
+
+  # Find the Canvas Integration URL in the Desk.com with the Aha name
+  def find_canvas_integration_url
+    response = client.integration_urls
+
+    loop do
+      response.entries.each do |integration_url|
+        return integration_url if integration_url.name == INTEGRATION_URL_NAME
+      end
+
+      # Get the next page if it exists
+      response = response.next
+
+      break unless response
+    end
+
+    return nil
+  end
+
+  def client
+    DeskApi::Client.new({
+      token: data.oauth_token,
+      token_secret: data.oauth_secret,
+      consumer_secret: data.consumer_secret,
+      consumer_key: data.consumer_key,
+      endpoint: "https://#{data.oauth_host}"
+    })
   end
 end
