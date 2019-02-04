38ba671c49a207854473ed91543b02b00abe21fc
Bill Chaney <bill@aha.io>
2019-02-04 15:40:57 -0800

    Update VSTS naming to Azure DevOps Services

--- lib/services/vso.rb
+++ lib/services/vso.rb
@@ -1,11 +1,11 @@
 class AhaServices::VSO < AhaService
-  title "Visual Studio Team Services"
-  caption "Send features and requirements to Microsoft Visual Studio Team Services"
+  title "Azure DevOps Services"
+  caption "Send features and requirements to Microsoft Azure DevOps Services"
   service_name "tfs"
 
-  string :account_name, description: "The name of your Visual Studio subdomain. e.g. if your Visual Studio domain is https://mycompany.visualstudio.com, then enter 'mycompany' here."
+  string :account_name, description: "The name of your Azure DevOps Services subdomain. e.g. if your Azure DevOps Services domain is https://fredwin.visualstudio.com or https://dev.azure.com/fredwin, then enter 'fredwin' here."
   string :user_name, description: "Enter your email address or username (if you use a personal access token this field is not used)."
-  password :user_password, description: "Enter a personal access token from Visual Studio Team Services."
+  password :user_password, description: "Enter a personal access token from Azure DevOps Services."
   
   include TfsCommon
   
