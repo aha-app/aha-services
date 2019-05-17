f6372093005247853f8926de0736e8eda77f84df
Yuko Takegoshi <vlyandra@gmail.com>
2019-05-17 18:08:51 -0400

    Allow bitbucket commit hook to process product prefixes with numbers

--- spec/services/bitbucket_webhook_spec.rb
+++ spec/services/bitbucket_webhook_spec.rb
@@ -15,4 +15,19 @@ describe AhaServices::BitbucketCommitHook do
     expect(comment_request.uri.path).to eq("/api/v1/features/ALEX-163/comments")
     expect(JSON.parse(comment_request.body)["comment"]["body"]).to match(/Alex Bartlow committed/)
   end
+  
+  it "processes with product prefixes with numbers" do
+    comment_request = nil
+    stub_request(:post, /.*/)
+      .with {|request| comment_request = request }
+      .to_return(status: 404, body: "", headers: {})
+  
+    AhaServices::BitbucketCommitHook.new(
+      {},
+      JSON.parse(fixture("bitbucket_commit_hook/bitbucket_commit_hook_webhook_numbers.json").read)
+    ).receive(:webhook)
+  
+    expect(comment_request.uri.path).to eq("/api/v1/features/BIG2-163/comments")
+    expect(JSON.parse(comment_request.body)["comment"]["body"]).to match(/Alex Bartlow committed/)
+  end
 end
