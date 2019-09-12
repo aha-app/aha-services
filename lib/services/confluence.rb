110c51caa306e0ff7da385a784d0f1a17c9db090
Phil Wilt <phillip.wilt@gmail.com>
2019-09-12 11:43:14 -0500

    Add a method to be included as a standard service

--- lib/services/confluence.rb
+++ lib/services/confluence.rb
@@ -1,6 +1,13 @@
 class AhaServices::Confluence < AhaService
   caption 'Embed a live Aha! roadmap, report, or presentation in your Confluence page.'
+
+  # Integrations in this category do not directly receive or send data but are rather
+  # presentational integrations from our shared webpages.
   category 'Wiki'
 
   confluence_button
+
+  def receive_wiki
+    # Placeholder class method allowing the service to be included in product services.
+  end
 end
